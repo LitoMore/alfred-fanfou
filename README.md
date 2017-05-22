eb007a260fbc6f84e366e3dfd2c98f0ddc10ce6a
LitoMore <litomore@gmail.com>
2017-05-23 04:42:51 +0800

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # alfred-fanfou
-Alfred Fanfou Workflow
+
+Alfred Fanfou
+
+## Installation
+
+```bash
+$ npm install -g alfred-fanfou
+```
+
+## Config & Login
+
+Use `ff config [CONUSMER_KEY] [CONUSMER_SECRET]` to config your app
+
+![](http://photo2.fanfou.com/v1/mss_3d027b52ec5a4d589e68050845611e68/ff/n0/0e/0w/v6_123495.jpg)
+
+Then use `ff login [USERNAME] [PASSWORD]` to login your account
+
+![](http://photo3.fanfou.com/v1/mss_3d027b52ec5a4d589e68050845611e68/ff/n0/0e/0w/v7_123494.jpg)
+
+## Post
+
+Use `ff say something!` to post
+
+Enjoy it! :]
---
+++ fanfou.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const homedir = require('homedir')
+const Fanfou = require('fanfou-sdk')
+const filePath = `${homedir()}/.alfred-fanfou/config.json`
+const arg = process.argv[2]
+const args = arg.split(' ')
+
+if (args[0] === 'config') {
+  const consumerKey = args[1]
+  const consumerSecret = args[2]
+
+  createConfig({
+    consumer_key: consumerKey,
+    consumer_secret: consumerSecret
+  })
+} else if (args[0] === 'login') {
+  const username = args[1]
+  const password = args[2]
+  const config = require(filePath)
+
+  const ff = new Fanfou({
+    auth_type: 'xauth',
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    username: username,
+    password: password
+  })
+  ff.xauth((e, res) => {
+    if (!e) {
+      const oauthToken = res.oauth_token
+      const oauthTokenSecret = res.oauth_token_secret
+      config.oauth_token = oauthToken
+      config.oauth_token_secret = oauthTokenSecret
+      createConfig(config)
+    }
+  })
+} else {
+  const text = process.argv
+  console.log(text)
+  const config = require(filePath)
+
+  const ff = new Fanfou({
+    auth_type: 'oauth',
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    oauth_token: config.oauth_token,
+    oauth_token_secret: config.oauth_token_secret
+  })
+
+  ff.post('/statuses/update', {status: text}, () => {})
+}
+
+function createConfig (content) {
+  fs.mkdir(`${homedir()}/.alfred-fanfou/`, () => {
+    fs.writeFileSync(filePath, JSON.stringify(content))
+  })
+}
