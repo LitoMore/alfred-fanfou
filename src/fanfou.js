0086b46f0e9a65509e6fe4afe6fdc3ffc291eaa5
LitoMore <litomore@gmail.com>
2018-08-29 18:22:14 +0800

    Bump fanfou-sdk@3.0.1

--- src/fanfou.js
+++ src/fanfou.js
@@ -53,18 +53,17 @@ if (args[0] === 'config') {
     protocol: config.https ? 'https:' : 'http:',
     fakeHttps: Boolean(config.https)
   })
-  ff.xauth(async (e, res) => {
-    if (e) {
-      console.log(e.message)
-    } else {
-      const oauthToken = res.oauth_token
-      const oauthTokenSecret = res.oauth_token_secret
+  ff.xauth()
+    .then(async res => {
+      const {oauthToken, oauthTokenSecret} = res
       config.oauth_token = oauthToken
       config.oauth_token_secret = oauthTokenSecret
       await createConfig(config)
       console.log('登录成功！')
-    }
-  })
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
 } else if (['h', 'm', 'me', 'p', 'undo'].indexOf(args[0]) === -1) {
   const text = argStr
   const config = require(filePath)
@@ -76,13 +75,13 @@ if (args[0] === 'config') {
     protocol: config.https ? 'https:' : 'http:',
     fakeHttps: Boolean(config.https)
   })
-  ff.post('/statuses/update', {status: text}, (err, res) => {
-    if (err) {
-      console.log(err.message)
-    } else {
+  ff.post('/statuses/update', {status: text})
+    .then(res => {
       console.log(res.text)
-    }
-  })
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
 } else {
   const config = require(filePath)
   const ff = new Fanfou({
@@ -94,44 +93,37 @@ if (args[0] === 'config') {
     fakeHttps: Boolean(config.https)
   })
   const count = args[1] || 10
-  const getTimeline = uri => {
-    ff.get(uri, {count}, (err, res) => {
-      if (err) {
-        output({items: [{title: '饭否', subtitle: err.message}]})
-      } else {
-        const timeline = []
-        res.forEach(item => {
-          timeline.push({
-            title: item.user.name,
-            subtitle: item.text,
-            mods: {
-              cmd: {
-                subtitle: pangu.spacing(new Timeago().format(item.created_at, 'zh_CN')) + ' via ' + item.source_name
-              }
-            },
-            quicklookur: 'https://fanfou.com'
-          })
+  const getTimeline = async uri => {
+    try {
+      const res = await ff.get(uri, {count})
+      const timeline = []
+      res.forEach(item => {
+        timeline.push({
+          title: item.user.name,
+          subtitle: item.text,
+          mods: {
+            cmd: {
+              subtitle: pangu.spacing(new Timeago().format(item.created_at, 'zh_CN')) + ' via ' + item.source_name
+            }
+          },
+          quicklookur: 'https://fanfou.com'
         })
-        output({items: timeline})
-      }
-    })
+      })
+      output({items: timeline})
+    } catch (err) {
+      output({items: [{title: '饭否', subtitle: err.message}]})
+    }
   }
 
-  const undo = () => {
-    ff.get('/statuses/user_timeline', {count: 1}, (err, res) => {
-      if (err) {
-        output({items: [{title: '饭否', subtitle: err.message}]})
-      } else {
-        const status = res[0] || {}
-        ff.post('/statuses/destroy', {id: status.id}, err => {
-          if (err) {
-            output({items: [{title: '饭否', subtitle: err.message}]})
-          } else {
-            console.log('删除成功！')
-          }
-        })
-      }
-    })
+  const undo = async () => {
+    try {
+      const userTimeline = await ff.get('/statuses/user_timleine', {count: 1})
+      const [status = {}] = userTimeline
+      await ff.post('/statuses/destroy', {id: status.id})
+      console.log('删除成功！')
+    } catch (err) {
+      output({items: [{title: '饭否', subtitle: err.message}]})
+    }
   }
 
   switch (args[0]) {
